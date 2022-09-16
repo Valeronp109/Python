63a681869db5e6e9f63dc70e8f300d358fb4cb38
Valeronp109 <113669424+Valeronp109@users.noreply.github.com>
2022-09-16 13:29:47 +0530

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,15 @@
+#Slicing
+
+a=[10,14,8,34,67,47,22]
+print(a)
+b=a[1:]
+print("slicing from first number to last",b)
+
+c=a[:3]
+print("slicing first 3 numbers",c)
+
+d=a[:-3]
+print("slicing last 3 numbers",d)
+
+e=a[2:6]
+print("slicing array from 2nd to 6th index:",e)
